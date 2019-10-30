1466df6e1fd1a0518a546e7040744b2634eed39c
AlexJanss <alex.janssen20@gmail.com>
2019-10-30 20:54:33 +0100

    new register form

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -2,10 +2,13 @@
     <form @submit.prevent="submit">
         <label for="first_name">First name:</label>
         <input id="first_name" v-model="form.first_name" />
+        <div v-if="$page.errors.first_name">{{ $page.errors.first_name[0] }}</div>
         <label for="last_name">Last name:</label>
         <input id="last_name" v-model="form.last_name" />
+        <div v-if="$page.errors.last_name">{{ $page.errors.last_name[0] }}</div>
         <label for="email">Email:</label>
         <input id="email" v-model="form.email" />
+        <div v-if="$page.errors.email">{{ $page.errors.email[0] }}</div>
         <button type="submit">Submit</button>
     </form>
 </template>
