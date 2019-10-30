a57dce7a7b366db3ae5f05d0456a7aa872c9e11a
AlexJanss <alex.janssen20@gmail.com>
2019-10-30 20:50:34 +0100

    changes to user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Users/Index', [
+            'users' => User::all(),
+        ]);
+    }
+
+    public function store()
+    {
+        User::create(
+            Request::validate([
+                'first_name' => ['required', 'max:50'],
+                'last_name' => ['required', 'max:50'],
+                'email' => ['required', 'max:50', 'email'],
+            ])
+        );
+
+        return Redirect::route('users');
+    }
+}
