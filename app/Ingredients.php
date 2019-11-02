5825695c358ed48bfddbff564f5c028703d36c0d
AlexJanss <alex.janssen20@gmail.com>
2019-11-02 07:47:24 +0100

    add relation to ingredients

--- app/Ingredients.php
+++ app/Ingredients.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredients extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function ingredients()
+    {
+        return $this->belongsToMany(Recipes::class);
+    }
 }
