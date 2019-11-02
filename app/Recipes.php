580145ec5b3f792ce17fbe7bc3c93f514b9bc4a2
AlexJanss <alex.janssen20@gmail.com>
2019-11-02 07:47:02 +0100

    add relation to recipes

--- app/Recipes.php
+++ app/Recipes.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class recipes extends Model
+class Recipes extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredients::class);
+    }
 }
