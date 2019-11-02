dc1c074927a255b7df2d7b4561c82ca28c1aead6
AlexJanss <alex.janssen20@gmail.com>
2019-11-02 07:49:53 +0100

    add missing migrations scripts DOUGH! and RecipeController

---
+++ database/migrations/2019_11_02_064300_create_recipe_ingredient_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipeIngredientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe_ingredient', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('recipe_id')->unsigned();
+            $table->integer('ingredient_id')->unsigned();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe_ingredient');
+    }
+}
