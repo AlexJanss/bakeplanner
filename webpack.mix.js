a281333bc3dcbf893184282e4e360c7246f67df1
AlexJanss <alex.janssen20@gmail.com>
2020-05-17 18:41:30 +0200

    update some config files

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,5 +16,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
-        postCss: [ tailwindcss('./path/to/your/tailwind.config.js') ],
+        postCss: [ tailwindcss('./tailwind.config.js') ],
     });
